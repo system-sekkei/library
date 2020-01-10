75e61a046db8b48c45d6aac56169cf37a346eeb8
Abe Asami <abeasa@abeasaminoMacBook-Air.local>
2020-01-10 18:18:17 +0900

    条件(図) 実装

---
+++ src/main/java/library/domain/model/LoanRestrictions.java
@@ -0,0 +1,29 @@
+package library.domain.model;
+
+/**
+ * 貸出制限
+ */
+public enum LoanRestrictions {
+    貸出５冊まで,
+    貸出７冊まで,
+    貸出４冊まで,
+    貸出不可;
+
+    public static LoanRestrictions from(DelayPeriod delayPeriod, MemberType memberType) {
+        if (memberType == MemberType.大人 && delayPeriod == DelayPeriod.遅延日数３日未満) {
+            return 貸出５冊まで;
+        }
+
+        if (memberType == MemberType.子供) {
+            if (delayPeriod == DelayPeriod.遅延日数３日未満) {
+                return 貸出７冊まで;
+            }
+
+            if (delayPeriod == DelayPeriod.遅延日数７日未満) {
+                return 貸出４冊まで;
+            }
+        }
+
+        return 貸出不可;
+    }
+}
