2beb700967beeab059e87d6cd38091e64972614e
Abe Asami <sammy@nocono.net>
2020-01-16 23:40:34 +0900

    貸出図書モデル整理

--- src/main/java/library/domain/model/bookonloan/BookOnLoan.java
+++ src/main/java/library/domain/model/bookonloan/BookOnLoan.java
@@ -1,14 +1,21 @@
 package library.domain.model.bookonloan;
 
 import library.domain.model.book.Book;
+import library.domain.model.member.Member;
 
 /**
  * 貸出図書
  */
 public class BookOnLoan {
+    Member member;
     Book book;
+    LoanDate loanDate;
 
-    public BookOnLoan(Book book) {
-        this.book = book;
+    public LoanPeriod loanPeriod() {
+        return LoanPeriod.loanPeriod(loanDate);
+    }
+
+    public DelayPeriod delayPeriod() {
+        return null;
     }
 }
