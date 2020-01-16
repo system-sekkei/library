54d4a9b72fa8ccf1e826137eee1b9e6cb8dda8a8
Abe Asami <sammy@nocono.net>
2020-01-16 17:22:40 +0900

    Web予約の状態実装

---
+++ src/main/java/library/domain/model/reserving/WebReservingReserveStatus.java
@@ -0,0 +1,9 @@
+package library.domain.model.reserving;
+
+/**
+ * Web予約中の状態
+ */
+public enum WebReservingReserveStatus {
+    未準備,
+    準備完了
+}
---
+++ src/main/java/library/domain/model/reserving/WebReservingStatus.java
@@ -0,0 +1,9 @@
+package library.domain.model.reserving;
+
+/**
+ * Web予約の状態
+ */
+public enum WebReservingStatus {
+    予約中,
+    未予約
+}
