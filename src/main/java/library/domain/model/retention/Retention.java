2ea23859691a9fe73b695fcfad45311c351cda47
Abe Asami <sammy@nocono.net>
2020-01-17 20:36:22 +0900

    取置に予約をもたせた

--- src/main/java/library/domain/model/retention/Retention.java
+++ src/main/java/library/domain/model/retention/Retention.java
@@ -1,11 +1,13 @@
 package library.domain.model.retention;
 
 import library.domain.model.bookcollection.BookCollection;
+import library.domain.model.reservation.Reservation;
 
 /**
  * 取置
  */
 public class Retention {
+    Reservation reservation;
     RetainedDate retainedDate;
     BookCollection bookCollection;
 
