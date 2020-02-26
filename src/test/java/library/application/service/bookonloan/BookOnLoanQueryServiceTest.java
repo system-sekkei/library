58d0a701358a0455aa6fc31e010ea96f201f5ddc
Abe Asami <sammy@nocono.net>
2020-02-26 16:08:29 +0900

    会員の現在の全貸出図書取得サービスのテスト追加

--- src/test/java/library/application/service/bookonloan/BookOnLoanQueryServiceTest.java
+++ src/test/java/library/application/service/bookonloan/BookOnLoanQueryServiceTest.java
@@ -9,6 +9,7 @@ import library.domain.model.bookcollection.BookCollectionCode;
 import library.domain.model.bookonloan.loan.BookOnLoan;
 import library.domain.model.bookonloan.loan.LoanDate;
 import library.domain.model.bookonloan.loaning.LoaningOfBookCollection;
+import library.domain.model.bookonloan.loaning.MemberAllBookOnLoans;
 import library.domain.model.bookonloan.returning.ReturnDate;
 import library.domain.model.bookonloan.returning.ReturningBookOnLoan;
 import library.domain.model.member.Member;
@@ -17,8 +18,7 @@ import library.domain.type.date.Date;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @LibraryDBTest
 class BookOnLoanQueryServiceTest {
@@ -39,7 +39,8 @@ class BookOnLoanQueryServiceTest {
 
     @Test
     void 貸出図書を取得できる() {
-        registerBookOnLoan(new BookCollectionCode("2-A"));
+        registerBookOnLoan(new BookCollectionCode("2-A"), 1);
+
         BookOnLoan bookOnLoan = bookOnLoanQueryService.findBookOnLoanByBookCollectionCode(new BookCollectionCode("2-A"));
 
         assertEquals(bookOnLoan.member().memberNumber().value(), 1);
@@ -48,7 +49,7 @@ class BookOnLoanQueryServiceTest {
     @Test
     void 返却された貸出図書は取得できない() {
         BookCollectionCode bookCollectionCode = new BookCollectionCode("2-B");
-        registerBookOnLoan(bookCollectionCode);
+        registerBookOnLoan(bookCollectionCode, 1);
         BookOnLoan bookOnLoan = bookOnLoanQueryService.findBookOnLoanByBookCollectionCode(bookCollectionCode);
         returnBookRecordService.registerReturnBook(new ReturningBookOnLoan(bookOnLoan, new ReturnDate(Date.from("2020-02-21"))));
 
@@ -57,8 +58,31 @@ class BookOnLoanQueryServiceTest {
         });
     }
 
-    private void registerBookOnLoan(BookCollectionCode bookCollectionCode){
-        Member member = memberQueryService.findMember(new MemberNumber(1));
+    @Test
+    void 会員が現在借りている全貸出図書を取得できる() {
+        registerBookOnLoan(new BookCollectionCode("2-A"), 2);
+
+        Member member = memberQueryService.findMember(new MemberNumber(2));
+        MemberAllBookOnLoans memberAllBookOnLoans = bookOnLoanQueryService.findMemberAllBookOnLoans(member);
+
+        assertEquals(memberAllBookOnLoans.bookOnLoans().numberOfBookOnLoans().value(), 1);
+    }
+
+    @Test
+    void 会員が現在借りている全貸出図書取得時に返却した貸出図書が含まれない() {
+        BookCollectionCode bookCollectionCode = new BookCollectionCode("2-B");
+        registerBookOnLoan(bookCollectionCode, 2);
+        BookOnLoan bookOnLoan = bookOnLoanQueryService.findBookOnLoanByBookCollectionCode(bookCollectionCode);
+        returnBookRecordService.registerReturnBook(new ReturningBookOnLoan(bookOnLoan, new ReturnDate(Date.from("2020-02-21"))));
+
+        Member member = memberQueryService.findMember(new MemberNumber(2));
+        MemberAllBookOnLoans memberAllBookOnLoans = bookOnLoanQueryService.findMemberAllBookOnLoans(member);
+
+        assertEquals(memberAllBookOnLoans.bookOnLoans().numberOfBookOnLoans().value(), 0);
+    }
+
+    private void registerBookOnLoan(BookCollectionCode bookCollectionCode, int memberNumber){
+        Member member = memberQueryService.findMember(new MemberNumber(memberNumber));
         BookCollection bookCollection = bookCollectionQueryService.findBookCollection(bookCollectionCode);
         LoaningOfBookCollection loaningOfBookCollection = new LoaningOfBookCollection(member, bookCollection, new LoanDate(Date.from("2020-02-20")));
         bookOnLoanRecordService.registerBookOnLoan(loaningOfBookCollection);
