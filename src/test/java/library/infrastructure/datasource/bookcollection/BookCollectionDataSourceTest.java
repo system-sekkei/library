ec81538511c21014da926acdbb69f6b648541d1e
Abe Asami <sammy@nocono.net>
2020-03-05 20:51:41 +0900

    貸出レコードが複数存在する蔵書取得が動かなかったのでSQLを修正

---
+++ src/test/java/library/infrastructure/datasource/bookcollection/BookCollectionDataSourceTest.java
@@ -0,0 +1,47 @@
+package library.infrastructure.datasource.bookcollection;
+
+import library.LibraryDBTest;
+import library.domain.model.bookcollection.BookCollection;
+import library.domain.model.bookcollection.BookCollectionCode;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@LibraryDBTest
+@AutoConfigureMockMvc
+class BookCollectionDataSourceTest {
+
+    @Autowired
+    BookCollectionDataSource bookCollectionDataSource;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void 貸出履歴が複数ある蔵書を取得できる() throws Exception {
+        mockMvc.perform(
+                post("/bookonloan/register")
+                        .param("memberNumber.value", "1")
+                        .param("bookCollectionCode.value", "2-A")
+                        .param("loanDate.value", "2020-02-14"));
+
+        mockMvc.perform(
+                post("/returnbook/register")
+                        .param("bookCollectionCode.value", "2-A")
+                        .param("returnDate.value", "2020-02-14"));
+
+        mockMvc.perform(
+                post("/bookonloan/register")
+                        .param("memberNumber.value", "1")
+                        .param("bookCollectionCode.value", "2-A")
+                        .param("loanDate.value", "2020-02-14"));
+
+        BookCollection bookCollection = bookCollectionDataSource.findBookCollection(new BookCollectionCode("2-A"));
+
+        assertEquals("2-A", bookCollection.bookCollectionCode().toString());
+    }
+}
