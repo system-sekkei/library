ad8538affd7b839ee2bdcc8de0abbce7418926e0
Abe Asami <sammy@nocono.net>
2020-03-07 13:10:54 +0900

    同一本の蔵書を取り扱うクラスを作成

---
+++ src/main/java/library/domain/model/bookcollection/SameBooksBookCollections.java
@@ -0,0 +1,19 @@
+package library.domain.model.bookcollection;
+
+import library.domain.model.book.Book;
+
+/**
+ * 同一本の蔵書リスト
+ */
+public class SameBooksBookCollections {
+    Book book;
+    BookCollections bookCollections;
+
+    public BookCollectionStatus bookCollectionStatus() {
+        if (bookCollections.list.stream().anyMatch(bookCollection ->
+            bookCollection.bookCollectionStatus == BookCollectionStatus.在庫中)) {
+            return BookCollectionStatus.在庫中;
+        }
+        return BookCollectionStatus.貸出中;
+    }
+}
