025fa4647f10f6c532566e12ccafa865f8b28b31
masuda220 <masuda220@gmail.com>
2020-05-14 17:35:49 +0900

    TODO追加：貸出の判定ロジックの表現の改善

--- src/main/java/library/domain/model/loan/history/LoanHistory.java
+++ src/main/java/library/domain/model/loan/history/LoanHistory.java
@@ -16,6 +16,7 @@ public class LoanHistory {
         this.returnRecords = returnRecords;
     }
 
+    // TODO 時系列として最後が貸出か返却かで判断したい　貸出記録の最後に日付と返却記録の最後の日付を比較：記録ない場合は貸出中ではない
     public boolean isLoaning() {
         return loanRecords.historyCount().minus(returnRecords.historyCount()) >= 1;
     }
