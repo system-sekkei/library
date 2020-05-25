394723b97cfdccc57140047d20abff39bf90e6f6
masuda220 <masuda220@gmail.com>
2020-05-25 11:28:13 +0900

    予約.取置貸出履歴テーブルを追加

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -109,6 +109,13 @@ CREATE TABLE 蔵書.取置中 (
     登録日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+--　取置を貸し出した記録
+CREATE TABLE 予約.取置貸出履歴 (
+    予約番号 INTEGER PRIMARY KEY ,
+    蔵書番号 VARCHAR(40) NOT NULL ,
+    登録日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (予約番号, 蔵書番号) REFERENCES 予約.取置履歴(予約番号, 蔵書番号)
+);
 -- 取置の期限切れ
 CREATE TABLE 予約.取置期限切れ履歴 (
    予約番号 INTEGER PRIMARY KEY REFERENCES 予約.取置履歴,
