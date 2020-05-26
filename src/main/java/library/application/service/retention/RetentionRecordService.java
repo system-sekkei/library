29efcb50cb256c06820b338fca61876899ff12f9
masuda220 <masuda220@gmail.com>
2020-05-26 09:09:32 +0900

    予約の進捗区分の導入：JIG ユースケース複合図で、状態を遷移させるユースケースの可視化

--- src/main/java/library/application/service/retention/RetentionRecordService.java
+++ src/main/java/library/application/service/retention/RetentionRecordService.java
@@ -5,11 +5,14 @@ import library.application.repository.RetentionRepository;
 import library.domain.model.item.Item;
 import library.domain.model.item.ItemNumber;
 import library.domain.model.item.bibliography.Book;
+import library.domain.model.reservation.Progress;
 import library.domain.model.reservation.reservation.Reservation;
 import library.domain.model.reservation.retention.BookMatching;
 import library.domain.model.reservation.retention.Retained;
 import library.domain.model.reservation.retention.Retention;
 import org.springframework.stereotype.Service;
+
+import static library.domain.model.reservation.Progress.*;
 
 /**
  * 取置登録サービス
@@ -37,27 +40,44 @@ public class RetentionRecordService {
      * 予約された本を取り置く
      */
     public void retain(Retention retention) {
+
+        ensure(未準備);
         retentionRepository.retained(retention);
 
         Retained retained = retentionRepository.findBy(retention.itemNumber());
         retentionNotification.retained(retained);
+        ensure(準備完了);
     }
 
     /**
      * 取り置いた蔵書を貸し出す(準備完了を消しこむ)
      */
     public void releaseForLoan(ItemNumber itemNumber) {
+        ensure(準備完了);
         Retained retained = retentionRepository.findBy(itemNumber);
         retentionRepository.released(retained);
+        ensure(消込済);
     }
 
     /**
      * 取置を期限切れにする(準備完了を消し込む）
      */
     public void releaseAndExpire(ItemNumber itemNumber) {
+        ensure(準備完了);
         Retained retained = retentionRepository.findBy(itemNumber);
         retentionRepository.released(retained);
         retentionRepository.expired(retained);
+        ensure(消込済);
+    }
+
+    /**
+     * 事前/事後の進捗状態を確認する
+     */
+    private void ensure(Progress progress) {
+        // 進捗状態の表現の実験
+        // TODO 状態の取得・判定・例外送出
+        // 進捗という関心事の表現が目的
+        // データの整合性のチェック目的ではない
     }
 
 }
---
+++ src/main/java/library/domain/model/reservation/Progress.java
@@ -0,0 +1,17 @@
+package library.domain.model.reservation;
+
+/**
+ * 予約の進捗
+ * TODO 進捗の状態遷移の表現の実験用
+ */
+public enum Progress {
+    未準備("予約があるが、未取置"),
+    準備完了("取置済、連絡済"),
+    消込済("貸出または取置期限切れにより取置を解放");
+
+    String description;
+
+    Progress(String description) {
+        this.description = description;
+    }
+}
