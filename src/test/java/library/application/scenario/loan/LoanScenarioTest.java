59981a1c4f7584cee319cd384678aa76a9331142
Abe Asami <sammy@nocono.net>
2021-08-12 17:49:18 +0900

    [WIP]テストメソッド追加

--- src/test/java/library/application/scenario/loan/LoanScenarioTest.java
+++ src/test/java/library/application/scenario/loan/LoanScenarioTest.java
@@ -36,6 +36,26 @@ class LoanScenarioTest {
         assertSame(loanability , Loanability.貸出可能);
     }
 
+    @Test
+    @Deprecated
+    void 視聴覚資料を貸し出すことができる() {
+        LoanRequest loanRequest =
+                generate(1, "2-A", "2020-02-20");
+        Loanability loanability = loanScenario.loanability(loanRequest);
+
+        assertSame(loanability , Loanability.貸出可能);
+    }
+
+    @Test
+    @Deprecated
+    void 五点を超える視聴覚資料を貸し出すことはできない() {
+        LoanRequest loanRequest =
+                generate(1, "2-A", "2020-02-20");
+        Loanability loanability = loanScenario.loanability(loanRequest);
+
+        assertSame(loanability , Loanability.貸出可能);
+    }
+
     // FIXME 貸出可能になる
     //@Test
     void 貸出中の所蔵品は貸し出すことができない() {
