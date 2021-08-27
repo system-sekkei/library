62432088e9a818d36181c17e54b2d69f53c8fd63
Abe Asami <sammy@nocono.net>
2021-08-27 17:22:56 +0900

    貸出のシナリオテスト追加

--- src/test/java/library/application/scenario/loan/LoanScenarioTest.java
+++ src/test/java/library/application/scenario/loan/LoanScenarioTest.java
@@ -37,12 +37,59 @@ class LoanScenarioTest {
     }
 
     @Test
-    void 視聴覚資料を貸し出すことができる() {
+    void 小学生以下は15点まで所蔵品を借りることができる() {
+        List<String> list = List.of("1-B", "1-C", "2-A", "2-B", "2-C", "2-D", "2-E", "2-F", "2-G", "2-H", "3-A", "4-A", "5-A", "6-A");
+        for (String code : list) {
+            LoanRequest loanRequest =
+                    generate(2, code, "2020-02-20");
+            loanScenario.loan(loanRequest);
+        }
+
         LoanRequest loanRequest =
-                generate(1, "8-A", "2020-02-20");
+                generate(2, "8-A", "2020-02-20");
+
         Loanability loanability = loanScenario.loanability(loanRequest);
 
-        assertSame(loanability , Loanability.貸出可能);
+        assertSame(loanability, Loanability.貸出可能);
+    }
+
+    @Test
+    void 中学生以上は20点まで所蔵品を借りることができる() {
+        List<String> list = List.of("1-B", "1-C", "2-A", "2-B", "2-C", "2-D", "2-E", "2-F", "2-G", "2-H", "3-A", "4-A",
+                "5-A", "6-A", "7-A", "8-A", "8-B", "9-A", "9-B");
+        for (String code : list) {
+            LoanRequest loanRequest =
+                    generate(3, code, "2020-02-20");
+            loanScenario.loan(loanRequest);
+        }
+
+        LoanRequest loanRequest =
+                generate(3, "10-A", "2020-02-20");
+
+        Loanability loanability = loanScenario.loanability(loanRequest);
+
+        assertSame(loanability, Loanability.貸出可能);
+    }
+
+    @Test
+    void 視聴覚資料を５点まで貸し出すことができる() {
+        List<LoanRequest> requests = List.of(
+                generate(1, "8-A", "2020-02-20"),
+                generate(1, "9-A", "2020-02-20"),
+                generate(1, "10-A", "2020-02-20"),
+                generate(1, "11-A", "2020-02-20"),
+                generate(1, "12-A", "2020-02-20")
+        );
+
+        Loanability loanability = null;
+        for (LoanRequest r : requests) {
+            loanability = loanScenario.loanability(r);
+            if (loanability == Loanability.貸出可能) {
+                loanScenario.loan(r);
+            }
+        }
+
+        assertSame(loanability, Loanability.貸出可能);
     }
 
     @Test
@@ -67,13 +114,13 @@ class LoanScenarioTest {
         assertSame(loanability, Loanability.視聴覚資料貸出不可);
     }
 
-    // FIXME 貸出可能になる
+    // FIXME:
     //@Test
     void 貸出中の所蔵品は貸し出すことができない() {
         LoanRequest loanRequest =
-                generate(2, "2-B", LoanDate.now().toString());
+                generate(2, "1-A", LoanDate.now().toString());
         Loanability loanability = loanScenario.loanability(loanRequest);
-        assertNotSame(loanability , Loanability.貸出可能);
+        assertSame(loanability , Loanability.貸出不可);
     }
 
     @Test
@@ -90,7 +137,7 @@ class LoanScenarioTest {
 
         Loanability loanability = loanScenario.loanability(loanRequest);
 
-        assertNotSame(loanability, Loanability.貸出可能);
+        assertSame(loanability, Loanability.貸出不可);
     }
 
     private LoanRequest generate(int memberNumber, String itemNumber, String loanDate) {
