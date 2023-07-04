259afad58f6c5efa9437015f8412e9ecf865bd10
setoguchi <setoguchi.takuma@gmail.com>
2023-07-04 16:31:50 +0900

    貸出日のテスト

---
+++ src/test/java/library/domain/model/loan/LoanDateTest.java
@@ -0,0 +1,22 @@
+package library.domain.model.loan;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 貸出日のテスト
+ */
+class LoanDateTest {
+
+    @Test
+    void 貸出日の表示文字列の取得() {
+
+        LoanDate sut = new LoanDate(LocalDate.parse("2023-07-01"));
+        String show = sut.show();
+        assertEquals("7月1日(土)", show);
+
+    }
+}
